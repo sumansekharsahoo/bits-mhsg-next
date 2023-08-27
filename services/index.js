1e5ba21c196442e74256ae9d95030d4c681811f5
Suman Sekhar Sahoo <sumansekharsahoo85@gmail.com>
2023-08-27 18:18:41 +0530

    latestt post orderby DESC

--- services/index.js
+++ services/index.js
@@ -234,8 +234,8 @@ export const getRecentPosts=async()=>{
     const query = gql`
         query GetPostDetails(){
             posts(
-                orderBy: date_ASC
-                last: 3
+                orderBy: date_DESC
+                first: 3
             ){
                 title
                 featuredImage{
