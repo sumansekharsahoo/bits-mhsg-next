e0ae28dc8d616539e944365e204899a7e5015501
Suman Sekhar Sahoo <sumansekharsahoo85@gmail.com>
2024-01-19 03:45:04 +0530

    minor update

--- components/index.js
+++ components/index.js
@@ -2,4 +2,4 @@ export {default as NavBar} from './NavBar'
 export {default as Layout} from './Layout'
 export {default as Footer} from './Footer'
 
-export const admins= ["loremipsuman85@gmail.com","f20212605@hyderabad.bits-pilani.ac.in","f20160450h@alumni.bits-pilani.ac.in","f20211608@hyderabad.bits-pilani.ac.in","f20213040@hyderabad.bits-pilani.ac.in","f20202487@hyderabad.bits-pilani.ac.in","mhsg@hyderabad.bits-pilani.ac.in","f20220003@hyderabad.bits-pilani.ac.in","f20221411@hyderabad.bits-pilani.ac.in","f20200157@hyderabad.bits-pilani.ac.in","f20221573@hyderabad.bits-pilani.ac.in","f20221248@hyderabad.bits-pilani.ac.in","f20220360@hyderabad.bits-pilani.ac.in"]
+export const admins= ["loremipsuman85@gmail.com","f20212605@hyderabad.bits-pilani.ac.in","f20160450h@alumni.bits-pilani.ac.in","f20211608@hyderabad.bits-pilani.ac.in","f20213040@hyderabad.bits-pilani.ac.in","f20202487@hyderabad.bits-pilani.ac.in","mhsg@hyderabad.bits-pilani.ac.in","f20220003@hyderabad.bits-pilani.ac.in","f20221411@hyderabad.bits-pilani.ac.in","f20200157@hyderabad.bits-pilani.ac.in","f20221573@hyderabad.bits-pilani.ac.in","f20221248@hyderabad.bits-pilani.ac.in","f20220360@hyderabad.bits-pilani.ac.in" ]
