3dc65d11f3dd2c6a077e01345905e4749a4f3151
Suman Sekhar Sahoo <sumansekharsahoo85@gmail.com>
2024-01-19 12:44:52 +0530

    conf mailto

--- components/ConfessCell.jsx
+++ components/ConfessCell.jsx
@@ -23,7 +23,8 @@ const ConfessCell = (props) => {
     }
     let edit= props.mark;
     let done= props.resolved
-    let adminedit=edit && !done
+    let adminedit=edit && !done;
+    const mailto= 'mailto:'+props.mail;
     return (
     <div className='confessContainer px-[12px] py-[8px] my-[10px] rounded-xl border-[2px] border-[#b0b0b0] lg:w-[70%] sm:w-[80%] w-[95%] flex flex-col align-middle text-[#474747]' style={{display}}>
         <div className='md:text-[24px] text-[20px] flex justify-between'><span className=''>{props.name}</span>{done?<span className='text-[#319c3a]'><GoCheckCircle  className='inline pb-[5px]'/> Resolved</span>:<span className='text-[#e1864a]'><GoClock className='inline'/> Pending</span>}</div>
@@ -35,7 +36,7 @@ const ConfessCell = (props) => {
             </span>
             <span>
                 {adminedit?<button onClick={markResolved} className='bg-[#eadb68] p-[5px] rounded-md md:text-[18px] text-[14px]'>mark resolved</button>:<></>}
-                {edit?<a href='mailto:mhsg@hyderabad.bits-pilani.ac.in'  target="_blank" className='border-[1px] border-[#b0b0b0] rounded-md px-[5px] py-[5px] md:text-[18px] text-[14px] bg-[#95dcbd]'>Resolve <GoArrowRight className='inline'/></a>:<button onClick={deleteConfess} className='text-[21px] delIcon sm:text-[24px]'><LuTrash /></button>}
+                {edit?<a href={mailto}  target="_blank" className='border-[1px] border-[#b0b0b0] rounded-md px-[5px] py-[5px] md:text-[18px] text-[14px] bg-[#95dcbd]'>Resolve <GoArrowRight className='inline'/></a>:<button onClick={deleteConfess} className='text-[21px] delIcon sm:text-[24px]'><LuTrash /></button>}
             </span>
 
         </div>
